e36b04cd16a0ff1fd5138a94f084e2f3e5ab3d11
Ajay Prasad P K <77564920+sudomaster00081@users.noreply.github.com>
2023-08-01 10:16:49 +0000

    And ML update

--- TFML/And ML.py
+++ TFML/And ML.py
@@ -1,39 +1,32 @@
 
 def predict(A, B, WA, WB):
     result = A*WA + B*WB
-    # if result < 0.5 :
-    #      result = 0
-    # else :
-    #      result = 1
+    if result < 0.5 :
+        result = 0
+    else :
+        result = 1
     return result
-
 input = [[0,0],
          [0,1],
          [1,0],
          [1,1]]
-
 expected_output = [0,0,0,1]
 weight = [0,0]
 flag = 0
 i=0
 lr = 0.1
 j=0
-while flag !=1:
-    
+while flag !=1:   
     for i in range (len(input)):
         A,B = input[i]
         WA, WB = weight
         predicted = predict(A,B, WA, WB)
-        error = expected_output[i] - predicted
-        
+        error = expected_output[i] - predicted       
         if error < 0: 
             error = error * -1 
         if (predicted != expected_output[i]):
             WA = WA + (lr * error * A*B)
-            WB = WB + (lr * error * A*B)
-        
-        
-        
+            WB = WB + (lr * error * A*B)     
     print(f"Iteration {j}\n Weight = [{WA,WB}]")
     j=j+1
     prev_weight = weight
